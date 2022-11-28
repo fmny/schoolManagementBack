90a7932e42313da5b3dc99ce8921ec0a4005b479
Francis Mauny <f_mauny@hotmail.fr>
2022-11-28 17:41:30 +0100

    dataInsert

--- src/main/java/com/thales/controller/DataInsertController.java
+++ src/main/java/com/thales/controller/DataInsertController.java
@@ -1,99 +1,123 @@
 package com.thales.controller;
-/*
-import com.thales.dao.CategorieDao;
+
+
+import com.thales.dao.*;
+import com.thales.model.Address;
+import com.thales.model.ClassRoom;
+import com.thales.model.Course;
+import com.thales.model.School;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
-//Voir + tard
+
 
 @RestController()
 @RequestMapping("/datainsert")
 public class DataInsertController {
     @Autowired
-    private CategorieDao categorieDao;
+    private AddressDao addressDao;
     @Autowired
-    private IndividuDao individuDao;
+    private ClassRoomDao classRoomDao;
     @Autowired
-    PieceDao pieceDao;
+    private CourseDao courseDao;
     @Autowired
-    VehiculeDao vehiculeDao;
+    private GradeDao gradeDao;
+    @Autowired
+    private LessonDao lessonDao;
+    @Autowired
+    private SchoolDao schoolDao;
+    @Autowired
+    private TeacherDao teacherDao;
+    @Autowired
+    private UserDao userDao;
+
 
     @GetMapping({"/", ""})
     public void insert(){
-        getListIndividu();
         getList();
     }
 
 
-    final public void getListIndividu() {
-
-        //Individu individu1 = new Individu("toto@gmail.com","0142231628","Dupont","Gérard","RECAP.2022!","ADMINISTRATIF");
-        Individu individu2 = new Individu("franck@gmail.com","0242223982","Dubosc","Franck","RECAP.2022!","GESTIONNAIRE");
-        Individu individu3 = new Individu("lili@gmail.com","0443431628","Bétancourt","Liliane","RECAP.2022!","MAGASINIER");
-        Individu individu4 = new Individu("josy@gmail.com","0443423628","Anne","Josy","RECAP.2022!","MAGASINIER");
-
-        Individu individu1 = new Individu("a@a.com","0142231628","Dupont","Gérard","","ADMINISTRATIF");
-
-
-
-        individuDao.save(individu1);
-        individuDao.save(individu2);
-        individuDao.save(individu3);
-        individuDao.save(individu4);
-
-    }
-
     final public void getList() {
-        Categorie categorie1 = new Categorie("ELEC","Electromobilité",new Date());
-        Categorie categorie2 = new Categorie("FILTRE","Filtre",new Date());
-        Categorie categorie3 = new Categorie("HUILE","Huiles",new Date());
-        Categorie categorie4 = new Categorie("OUTIL","Outillage",new Date());
-        Categorie categorie5 = new Categorie("PNEU","Pneu",new Date());
 
-        categorieDao.save(categorie1);
-        categorieDao.save(categorie2);
-        categorieDao.save(categorie3);
-        categorieDao.save(categorie4);
-        categorieDao.save(categorie5);
+        Address address1=new Address("Nantes","rue des 4",1,"France");
+        Address address2=new Address("Nantes","rue des 5",1,"France");
+        addressDao.save(address1);
+        addressDao.save(address2);
 
-        Vehicule vehicule1 = new Vehicule("CLIO4","RENAULT","4",1,2010,5);
-        Vehicule vehicule2 = new Vehicule("FORDFIESTA","FORD","Fiesta",3,2015,3);
-        Vehicule vehicule3 = new Vehicule("FIAT500","FIAT","XS",11,2018,2);
-        Vehicule vehicule4 = new Vehicule("FIATPUNTO","FIAT","2",2,2020,1);
+        School school1=new School("etablissement1","Lycée","012345678","logo1");
+        School school2=new School("etablissement2","Collège","012345678","logo2");
 
-        vehiculeDao.save(vehicule1);
-        vehiculeDao.save(vehicule2);
-        vehiculeDao.save(vehicule3);
-        vehiculeDao.save(vehicule4);
+        school1.setAddress(address1);
+        school2.setAddress(address2);
 
-        Piece piece1 = new Piece("EL/205/12/15",1250, "BLANC",125f,155f,categorie1);
-        Piece piece2 = new Piece("FI/402/12/16",250, "BLEU",24f,37f,categorie2);
-        Piece piece3 = new Piece("HU/205/12/17",350, "NOIR",45f,55f,categorie3);
-        Piece piece4 = new Piece("OU/205/12/17",750, "NOIR",45f,55f,categorie4);
+        schoolDao.save(school1);
+        schoolDao.save(school2);
+
+        ClassRoom classRoom1=new ClassRoom("maclasse1",20);
+        ClassRoom classRoom2=new ClassRoom("maclasse2",25);
+        ClassRoom classRoom3=new ClassRoom("maclasse3",30);
+        ClassRoom classRoom4=new ClassRoom("maclasse4",35);
+        ClassRoom classRoom5=new ClassRoom("maclasse5",22);
+        ClassRoom classRoom6=new ClassRoom("maclasse6",21);
+        ClassRoom classRoom7=new ClassRoom("maclasse7",27);
+
+        classRoom1.setSchool(school1);
+        classRoom2.setSchool(school1);
+        classRoom3.setSchool(school1);
+        classRoom4.setSchool(school1);
+
+        classRoom5.setSchool(school2);
+        classRoom6.setSchool(school2);
+        classRoom7.setSchool(school2);
+
+        classRoomDao.save(classRoom1);
+        classRoomDao.save(classRoom2);
+        classRoomDao.save(classRoom3);
+        classRoomDao.save(classRoom4);
+        classRoomDao.save(classRoom5);
+        classRoomDao.save(classRoom6);
+        classRoomDao.save(classRoom7);
+
+        Course course1=new Course("Maths","blue");
+        Course course11=new Course("Maths","blue");
+        Course course2=new Course("Physique","red");
+        Course course21=new Course("Physique","red");
+        Course course3=new Course("Chimie","green");
+        Course course31=new Course("Chimie","green");
+        Course course4=new Course("Biologie","grey");
+        Course course5=new Course("EPS","pink");
+        Course course6=new Course("Anglais","yellow");
+        Course course7=new Course("Français","orange");
+        Course course71=new Course("Français","orange");
+        Course course8=new Course("Philosophie","purple");
+        Course course9=new Course("Allemand","crimson");
+        Course course10=new Course("Latin","black");
+
+        course1.setSchool(school1);
+        course2.setSchool(school1);
+        course3.setSchool(school1);
+        course4.setSchool(school1);
+        course5.setSchool(school1);
+
+        course11.setSchool(school2);
+        course21.setSchool(school2);
+        course31.setSchool(school2);
+        course7.setSchool(school2);
 
 
-        piece1.getVehicules().add(vehicule1);
-        piece1.getVehicules().add(vehicule2);
 
-        piece2.getVehicules().add(vehicule3);
-        piece2.getVehicules().add(vehicule2);
 
-        piece3.getVehicules().add(vehicule4);
-        piece3.getVehicules().add(vehicule2);
 
-        piece4.getVehicules().add(vehicule1);
-        piece4.getVehicules().add(vehicule4);
 
-        pieceDao.save(piece1);
-        pieceDao.save(piece2);
-        pieceDao.save(piece3);
-        pieceDao.save(piece4);
+
+
+
+
 
     }
-
 
 
 }
-*/
